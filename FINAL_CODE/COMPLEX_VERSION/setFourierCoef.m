86a5aa0be6821c39c7727ffd200a3f48a936f4e4
AARON HAGSTROM <52719949+aphagstrom@users.noreply.github.com>
2023-09-26 11:17:05 -0500

    Delete FINAL_CODE directory

--- FINAL_CODE/COMPLEX_VERSION/setFourierCoef.m
+++
@@ -1,5 +0,0 @@
-function A = setFourierCoef(n,a,A,N)
-
-A(N+1+n) = a;
-
-return
--- FINAL_CODE/COMPLEX_VERSION/simp2.m
+++
@@ -1,9 +0,0 @@
-function T = simp2(f,a,b,N,i)
-sum = 0;
-numPoints = 10^4;
-step = (b-a)/N; 
-ex = @(x)(exp(-1i*i*x));
-
-sum=sum+f(1)*ex(a)+f(end)*ex(b)+4*f(5001)*ex(0);
-T = step./6*(sum);
-return 
